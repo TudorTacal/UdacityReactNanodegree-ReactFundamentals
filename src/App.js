81b0755dcce9d3fb2c01b2ecc0785576d0b9bb3c
Tudor Tacal <tudortacal@gmail.com>
2017-10-30 18:12:42 +0000

    Adds a Link to React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+import CreateContacts from './CreateContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
+  state = {
+    screen: 'list',
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({contacts})
+    })
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter( (c) => c.id !== contact.id  )
+    }))
+
+    ContactsAPI.remove(contact)
+  }
   render() {
     return (
-      <div>
-        Hello World
+      <div className='app'>
+        {this.state.screen === 'list' && (
+          <ListContacts 
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+            onNavigate={() => {
+              this.setState({ screen: 'create' })
+            }}
+          />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContacts/>
+        )}
       </div>
     )
   }
